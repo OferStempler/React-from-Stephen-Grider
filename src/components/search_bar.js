62b6c8cdb4650fb72859a21ba0ca2c12cd53699a
Ofer <oferstemp@gmail.com>
2017-10-03 10:03:42 +0300

    state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = { term: ""};
+
+    }
+
+
     render () {
-        return <input />;
+        return (
+        <div>
+
+            <input onChange={event => this.setState({ term: event.target.value})} />;
+        Value of the input: {this.state.term}
+        </div>
+        )
+            }
+
+    onInputChange (event){
+        console.log(event.target.value);
     }
 }
 
